d5c3d5be24c1bca017d5491cd849bb45a7c2b99e
Aashish Parajuli <aashishparajuli018@gmail.com>
2019-12-18 13:13:05 +0545

    i have change qn 3

--- qn4.py
+++ qn4.py
@@ -1,9 +1,9 @@
 import re
 
-# with open('genius_club'.txt) as file :
-#    reader=file.read()
-#    print(reader)
+with open('genius_club.txt') as file :
+    reader=file.read()
 
-pattern=r'[+\d{3}-\d{10}|+\d{2}-\d{6}]'
-sequence=' asdjshd +977-9841585917 +01-565859 asdksjad  asdjshd +977-9841585917 +01-565859 asdksjad asdjshd +977-9841585917 +01-565859 asdksjad asdjshd +977-9841585917 +01-565859 asdksjad asdjshd +977-9841585917 +01-565859 asdksjad asdjshd +977-9841585917 +01-565859 asdksjad '
-print(re.findall(pattern,sequence))
+pattern=r'\+\d{3}\-\d{10}|\+\d{2}\-\d{6}'
+#sequence=' asdjshd +977-9841585917 +01-565859 asdksjad  asdjshd +977-9841585917 +01-565859 asdksjad asdjshd +977-9841585917 +01-565859 asdksjad asdjshd +977-9841585917 +01-565859 asdksjad asdjshd +977-9841585917 +01-565859 asdksjad asdjshd +977-9841585917 +01-565859 asdksjad '
+print(re.findall(pattern,reader))
+
